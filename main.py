96f9860a06d984943e0c2d763b07f4cf7d7f83e4
yeyevtushenko <yehor803@gmail.com>
2024-01-01 22:32:20 +0200

    Реалізація меню

---
+++ main.py
@@ -0,0 +1,30 @@
+def main():
+    numbers = []
+
+    while True:
+        print("\nМеню:")
+        print("1. Додати нове число до списку")
+        print("2. Видалити усі входження числа зі списку")
+        print("3. Показати вміст списку")
+        print("4. Перевірити, чи є значення у списку")
+        print("5. Замінити значення у списку")
+        print("0. Вийти з програми")
+
+        choice = input("Виберіть опцію (0 вихід з програми): ")
+        if choice == "1":
+            add_number(numbers)
+        elif choice == "2":
+            remove_number(numbers)
+        elif choice == "3":
+            show_list(numbers)
+        elif choice == "4":
+            check_value(numbers)
+        elif choice == "5":
+            replace_value(numbers)
+        elif choice == "6":
+            break
+        else:
+            print("Неправильний вибір. Спробуйте ще раз.")
+
+
+
